f59709bc68fc883852530dae5d89616db8c034ca
Hussain Naeem <husain.sims@gmail.com>
2020-01-25 12:45:41 +0500

    Updates

---
+++ precache-manifest.d07af071b7a115545935fc3339408b25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f3b4f04dc23243a8d6308f9ee6893f",
+    "url": "/Naqel-Client/index.html"
+  },
+  {
+    "revision": "f1929913a6896301521a",
+    "url": "/Naqel-Client/static/css/main.4c414e65.chunk.css"
+  },
+  {
+    "revision": "e107d8989245847125f6",
+    "url": "/Naqel-Client/static/js/2.ecc4ffe1.chunk.js"
+  },
+  {
+    "revision": "67809d07765aa63f755750ed313fe988",
+    "url": "/Naqel-Client/static/js/2.ecc4ffe1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f1929913a6896301521a",
+    "url": "/Naqel-Client/static/js/main.1cc9c210.chunk.js"
+  },
+  {
+    "revision": "327a4f1f41dd1d49b524",
+    "url": "/Naqel-Client/static/js/runtime-main.1c2fb77c.js"
+  }
+]);
