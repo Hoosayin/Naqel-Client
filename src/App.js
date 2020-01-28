1f1d36675de8614148b0d519a2dd943b0ca48a6f
Hussain Naeem <husain.sims@gmail.com>
2020-01-29 00:56:24 +0500

    Updated Router to HashRouter.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter, BrowserRouter as Router, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Landing from "./components/Landing";
@@ -15,7 +15,7 @@ import Footer from "./components/Footer";
 class App extends Component {
     render() {
         return (
-            <Router>
+            <HashRouter basename="/">
                 <div className="App">
                     <Header />
                     <Route exact path="/" component={Landing} />
@@ -28,7 +28,7 @@ class App extends Component {
                     <BackToTop />
                     <Footer />
                 </div>
-            </Router>
+            </HashRouter>
         );
     }
 }
