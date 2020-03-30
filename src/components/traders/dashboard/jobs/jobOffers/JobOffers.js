6b034fc492fef10ffa40ae4ed958ad598b1272ea
Hussain Naeem <husain.sims@gmail.com>
2020-03-30 15:26:18 +0500

    Some minor fixes.

--- src/components/traders/dashboard/jobs/jobOffers/JobOffers.js
+++ src/components/traders/dashboard/jobs/jobOffers/JobOffers.js
@@ -25,21 +25,21 @@ class JobOffers extends Component {
     render() {
         return (
             <section>
-                <div class="jumbotron theme-alt" style={{ width: "100%", backgroundColor: "#202020" }}>
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-12 col-md-push-12 text-center">
-                                <img class="img-responsive visible-xs-inline-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block visible-xl-inline-block"
+                <div className="jumbotron theme-alt" style={{ width: "100%", backgroundColor: "#202020" }}>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12 col-md-push-12 text-center">
+                                <img className="img-responsive visible-xs-inline-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block visible-xl-inline-block"
                                     alt="job_offers.png" src="./images/job_offers.png" data-source-index="2" />
                             </div>
-                            <div class="col-md-12 col-md-pull-12">
-                                <div class="type-h3">Job Offers</div>
-                                <div class="type-sh3">Manage Your Job Offers</div>
+                            <div className="col-md-12 col-md-pull-12">
+                                <div className="type-h3">Job Offers</div>
+                                <div className="type-sh3">Manage Your Job Offers</div>
                                 <p>Hi There! Want to transport your freight somewhere? Why not create a new job offer now!!</p>
-                                <div class="btn-group">
+                                <div className="btn-group">
                                     <button
                                         type="button"
-                                        class="btn btn-primary"
+                                        className="btn btn-primary"
                                         data-toggle="modal"
                                         data-target="#add-job-offer-dialog"
                                         onMouseDown={() => {
--- src/components/traders/dashboard/jobs/onGoingJobs/OnGoingJobs.js
+++ src/components/traders/dashboard/jobs/onGoingJobs/OnGoingJobs.js
@@ -4,16 +4,16 @@ class OnGoingJobs extends Component {
    render() {
         return (
             <section>
-                <div class="jumbotron theme-alt" style={{ width: "100%", backgroundColor: "#202020" }}>
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-12 col-md-push-12 text-center">
-                                <img class="img-responsive visible-xs-inline-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block visible-xl-inline-block"
+                <div className="jumbotron theme-alt" style={{ width: "100%", backgroundColor: "#202020" }}>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12 col-md-push-12 text-center">
+                                <img className="img-responsive visible-xs-inline-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block visible-xl-inline-block"
                                     alt="on_going_jobs.png" src="./images/on_going_jobs.png" data-source-index="2" />
                             </div>
-                            <div class="col-md-12 col-md-pull-12">
-                                <div class="type-h3">On-Going Jobs</div>
-                                <div class="type-sh3">Under Construction</div>
+                            <div className="col-md-12 col-md-pull-12">
+                                <div className="type-h3">On-Going Jobs</div>
+                                <div className="type-sh3">Under Construction</div>
                                 <p>For now, it's lonely here!</p>
                             </div>
                         </div>
